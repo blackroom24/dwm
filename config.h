7b4958f22f5a1e012f4226484ed12729c9651c58
Sanket Debnath <sanketdebnath24@gmail.com>
2020-05-13 21:55:33 +0530

    Changed terminal to kitty

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static const char *mutecmd[]  = { "mute", NULL };
 static const char *volupcmd[]  = { "volup", NULL };
