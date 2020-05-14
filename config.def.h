8c498dcf760d2375267ae1b2fbc326e527df3564
Sanket Debnath <sanketdebnath24@gmail.com>
2020-05-14 23:13:49 +0530

    utility changes

--- config.def.h
+++ config.def.h
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-p","run->",NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 static const char *mutecmd[]  = { "mute", NULL };
